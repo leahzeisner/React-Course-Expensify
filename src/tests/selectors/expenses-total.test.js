34705bed96f41b2f7c0cfa7bca9ba34307369b5e
leahzeisner <zeisner.l@northeastern.edu>
2021-05-27 18:24:49 -0400

    add expense summary to dashboard

---
+++ src/tests/selectors/expenses-total.test.js
@@ -0,0 +1,20 @@
+import selectExpensesTotal from "../../selectors/expenses-total";
+import expenses from "../fixtures/expenses";
+
+
+test('should return 0 if there are no expenses', () => {
+    const response = selectExpensesTotal([]);
+    expect(response).toBe(0);
+});
+
+
+test('should correctly add up a single expense', () => {
+    const response = selectExpensesTotal([expenses[0]]);
+    expect(response).toBe(195);
+});
+
+
+test('should correctly add up multiple expenses', () => {
+    const response = selectExpensesTotal(expenses);
+    expect(response).toBe(114195);
+});
