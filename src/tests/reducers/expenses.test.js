9107d4a51438454d0d2391c78e6da2cee27536fc
leahzeisner <zeisner.l@northeastern.edu>
2021-05-28 12:56:13 -0400

    get firebase expenses on start

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -77,3 +77,14 @@ test('should NOT edit expense if id not found', () => {
 
     expect(state).toEqual(expenses);
 });
+
+
+test('should set expenses', () => {
+    const action = {
+        type: 'SET_EXPENSES',
+        expenses: [expenses[1]]
+    };
+    const state = expensesReducer(expenses, action);
+
+    expect(state).toEqual([expenses[1]]);
+});
